fb89ec701d7242e58c109495f5e4ae84617c7c26
vinodhanvino <vinodhanshiva@gmail.com>
2025-01-28 10:47:13 +0530

    updates

--- b-d/app/users/user_db.py
+++ b-d/app/users/user_db.py
@@ -1,6 +1,9 @@
 from app.config.connection import DBCtrl
 from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_json import mutable_json_type
+from sqlalchemy.dialects.postgresql import JSONB
+
 import datetime
 import pytz
 
@@ -28,7 +31,10 @@ class User(Base):
     created = Column(DateTime, default=datetime.datetime.now(IST))
     modified = Column(DateTime, default=datetime.datetime.now(IST))
     archived = Column(Boolean, default=False)
+    more = Column(mutable_json_type(dbtype=JSONB, nested=True), default={})
 
-
-
+    @staticmethod
+    def addUser(**kwargs):
+        user = User()
+        user.username = ''
 Base.metadata.create_all(bind=DBCtrl().dbengine, checkfirst=True)
--- b-d/app/users/user_routes.py
+++ b-d/app/users/user_routes.py
@@ -1,7 +1,35 @@
-from fastapi import APIRouter
-
+from typing import Optional
+from fastapi import APIRouter, Request, HTTPException
+from pydantic import BaseModel
+from user_db import User
 
 router = APIRouter()
 PREFIX = '/api/v1/users_'
 
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    email: str
+    bio: str
+    profile_picture: str
+    interest: list[str]
+    more: Optional[dict] = {}
 
+class Users(UserCreate):
+    id : int
+
+@router.post('/add_user', response_model=User)
+def AddUser(request:Request, body:UserCreate):
+    try:
+
+        user = User.addUser(
+            username=body.username,
+            password = body.password,
+            email = body.email,
+            bio = body.bio,
+            profile_picture = body.profile_picture,
+            interest = body.interest,
+            more = body.more
+        )
+    except:
+        raise HTTPException()
